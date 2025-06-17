614e142443357ed0f2f3fc3b552b20eea5a50d58
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-06-17 11:58:34 +0800

    improvement : add editors and reviewers column

--- src/Pages/SubmissionReportPage.php
+++ src/Pages/SubmissionReportPage.php
@@ -2,39 +2,16 @@
 
 namespace SubmissionReport\Pages;
 
-use App\Actions\Submissions\SubmissionCreateAction;
-use App\Actions\Submissions\SubmissionUpdateAction;
-use App\Forms\Components\TinyEditor;
 use App\Models\Author;
-use App\Models\Enums\SubmissionStage;
 use App\Models\Enums\SubmissionStatus;
-use App\Models\Proceeding;
 use App\Models\Submission;
 use App\Models\Topic;
-use App\Models\Track;
-use App\Panel\ScheduledConference\Livewire\Submissions\Components\ContributorList;
-use App\Panel\ScheduledConference\Livewire\Submissions\Components\GalleyList;
 use Filament\Forms\Components\CheckboxList;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Livewire;
-use Filament\Forms\Components\Radio;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
-use Filament\Forms\Components\TagsInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
-use Filament\Forms\Get;
-use Filament\Notifications\Notification;
 use Filament\Pages\Page;
-use Filament\Tables\Concerns\InteractsWithTable;
-use Filament\Tables\Contracts\HasTable;
 use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\HtmlString;
 use OpenSpout\Common\Entity\Row;
@@ -58,6 +35,8 @@ class SubmissionReportPage extends Page implements HasForms
             'columns' => [
                 'id',
                 'authors',
+                'editors',
+                'reviews',
                 'submitter_name',
                 'submitter_email',
                 'submitter_affiliation',
@@ -104,7 +83,7 @@ class SubmissionReportPage extends Page implements HasForms
             ->schema([
                 CheckboxList::make('status')
                     ->label('Select Submission Status that you want to export')
-                    ->options(array_combine(SubmissionStatus::values(), SubmissionStatus::values()))
+                    ->options(collect(array_combine(SubmissionStatus::values(), SubmissionStatus::values()))->filter(fn($value) => !in_array($value, ['Payment Declined', 'On Payment'])))
                     ->bulkToggleable()
                     ->required(),
                 CheckboxList::make('columns')
@@ -113,10 +92,11 @@ class SubmissionReportPage extends Page implements HasForms
                     ->options([
                         'id' => 'ID',
                         'authors' => 'Authors',
+                        'editors' => "Editors",
+                        'reviews' => "Reviewers",
                         'submitter_name' => 'Submitter Name',
                         'submitter_email' => 'Submitter Email',
                         'submitter_affiliation' => 'Submitter Affiliation',
-                        // 'submitter_country_id' => 'Submitter Country ID',
                         'submitter_country' => 'Submitter Country',
                         'title' => "Submission Title",
                         'status' => "Submission Status",
@@ -140,6 +120,7 @@ class SubmissionReportPage extends Page implements HasForms
                 'meta',
                 'participants',
                 'authors',
+                'editors.user',
                 'user',
                 'topics',
             ])
@@ -174,6 +155,8 @@ class SubmissionReportPage extends Page implements HasForms
         return match($column){
             'id' => $submission->getKey(),
             'authors' => $submission->authors->implode(fn(Author $author) => $author->full_name, ', '),
+            'editors' => $submission->editors->implode(fn($editor) => $editor->user->full_name, ', '),
+            'reviewers' => $submission->reviews->implode(fn($review) => $review->user->full_name, ', '),
             'submitter_name' => $submission->user->full_name,
             'submitter_email' =>  $submission->user->email,
             'submitter_affiliation' => $submission->user->getMeta('affiliation'),
